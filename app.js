3f25b91574e60201b33f9fe99ad8334ea124b2cf
Administrator <administrator@macbook-pro-de-administrator.home>
2019-05-20 19:51:11 +0200

    remove cluster

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
 import bodyParser from 'body-parser';
-import cluster from 'cluster';
-import { cpus } from 'os';
 
 const app = express();
 const server = require('http').createServer(app);
@@ -12,17 +10,7 @@ app.disable('x-powered-by');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-if (cluster.isMaster) {
-    for (let i = 0; i < cpus().length; i++) {
-      cluster.fork();
-    }
-    cluster.on('exit', (worker, code, signal) => {
-      console.log(`worker ${worker.process.pid} died`);
-    });
-  } else {
-    createRouter(app);
-    server.listen(process.env.port, () => {
-        console.log(`listening on port ${process.env.port}`);
-    });
-    console.log(`Worker ${process.pid} started`);
-  }
+createRouter(app);
+server.listen(process.env.port, () => {
+    console.log(`listening on port ${process.env.port}`);
+});
